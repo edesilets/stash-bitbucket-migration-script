74f557de38f6e7c2dd02e8af1fa180ab5a63b07e
Ethan Desilets <ethan.desilets@hhcc.com>
2018-02-05 11:18:44 -0500

    Extract stashy module to it's own class

--- stash.py
+++ stash.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python
-import sys
-import os
-import settings
 import stashy
-import pprint
 
-stash_username = os.environ.get("STASH_USERNAME")
-stash_password = os.environ.get("STASH_PASSWORD")
-stash_host     = os.environ.get("STASH_HOST")
+class Stash():
+    def __init__(self, host, username, password):
+        self.stash = stashy.connect(host, username, password)
 
-stash = stashy.connect(stash_host, stash_username, stash_password)
+    def getProjects(self):
+        return self.stash.projects.list()
 
-projects = stash.projects.list()
-
-# /Users//Documents/python/stash/delete
-starting_dir = os.getcwd() + '/' + 'delete/'
-
-# Projects
-for project in projects:
-    pprint.pprint('Folder Name: ' + project['name'])
-    pprint.pprint('Project Key: ' + project['key'])
-    project_key = project['key']
-
-    projectsRepos = stash.projects[project_key].repos
-    project_repositories = projectsRepos.list()
-
-    # pprint.pprint(project_repositories)
-    # print '\n'
-    for repository in project_repositories:
-        pprint.pprint('Project Name: ' + repository['name'])
-        pprint.pprint('Git URL: ' + repository['links']['clone'][0]['href'])
-        path = starting_dir + project['name'] +'/'+ repository['name']
-        print path
-        os.makedirs(path)
-        os.chdir(path)
-        os.system("git clone %s" % repository['links']['clone'][0]['href'])
-        os.chdir(starting_dir)
-        print '\n'
+    def getProjectRepositories(self, project_key):
+        return self.stash.projects[project_key].repos
