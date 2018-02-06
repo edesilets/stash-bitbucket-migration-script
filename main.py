7e5db4ff6eb019471457fd8132c90f96ab2e9631
Ethan Desilets <ethan.desilets@hhcc.com>
2018-02-06 11:24:55 -0500

    Push local repo to bitbucket. Breaks after one repo push. Testing still.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import stash
 import settings
 import pprint
 from urlparse import urlparse
+from git import Repo
+from git import Git
 
 bitbucket_username = os.environ.get("BITBUCKET_USERNAME")
 bitbucket_password = os.environ.get("BITBUCKET_PASSWORD")
@@ -32,15 +34,50 @@ def setupBitBucketRepository(projectKey, name):
     bb.setRepositoryPermissions(projectKey, repositoryKey, "user", "Ethan.Desilets", 'admin')
     return repositoryGitUrl
 
-def saveRepositoriesLocally(repository, project):
-        pprint.pprint('Project Name: ' + repository['name'])
-        pprint.pprint('Git URL: ' + repository['links']['clone'][0]['href'])
-        path = starting_dir + project['name'] +'/'+ repository['name']
-        print path
-        # os.makedirs(path)
-        # os.chdir(path)
-        # os.system("git clone %s" % repository['links']['clone'][0]['href'])
-        # os.chdir(starting_dir)
+def uploadToBitBucket(git_remote_url, git_folder_path):
+        cleanURL = gitUrlParse(git_remote_url, 'bitBucketGit')
+        add_remote = "git remote add bitbucket %s" % cleanURL
+        pprint.pprint(add_remote)
+        os.system(add_remote)
+        os.system("git fetch origin")
+
+        git_ssh_identity_file = os.path.expanduser('~/.ssh/bitBucket')
+        git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
+        pprint.pprint(git_ssh_cmd)
+        with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
+            pprint.pprint("Pushing to bitBucket!!!")
+            Repo(git_folder_path).remote(name='bitbucket').push('master')
+            pprint.pprint("Pushing to COMPLETE!!!")
+
+        # repo   = Repo(gitFolderPath)
+        # ssh_cmd = 'ssh -i /Users/ethan.desilets/.ssh/bitBucket'
+        # with repo.git.custom_environment(GIT_SSH_COMMAND=ssh_cmd):
+        #     # repo.remotes.origin.fetch()
+        #     repo.remote(name='bitbucket').push()
+
+def saveRepositoriesLocally(repository, project, bitBucketGitUrl):
+        pprint.pprint('Stash Git URL: ' + repository['links']['clone'][0]['href'])
+
+        path = starting_dir + project['name']
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        pprint.pprint("Local Path: " + path)
+        os.chdir(path)
+
+        os.system("git clone %s" % repository['links']['clone'][0]['href'])
+
+        # hyphens and lower case missing from repo name to git clone folder name
+        gitFolderPath = path+"/"+repository['name'].lower().replace(" ","-")
+
+        os.chdir(gitFolderPath)
+        pprint.pprint("Directory after clone:  " +os.getcwd())
+
+        uploadToBitBucket(bitBucketGitUrl, gitFolderPath)
+
+        os.chdir(starting_dir)
+        pprint.pprint("Directory after getting ready for next clone:  " +os.getcwd())
         print '\n'
 
 bb       = bitbucket.BitBucket(bitbucket_host, bitbucket_username, bitbucket_password)
@@ -61,5 +98,12 @@ for project in projects:
 
     # pprint.pprint(project_repositories)
     # print '\n'
+    setupBitBucketProject(project_key, project['name'])
+
     for repository in project_repositories:
-        saveRepositoriesLocally(repository, project)
+        pprint.pprint('Stash Project Name: ' + repository['name'])
+        bitBucketGitUrl = setupBitBucketRepository(project_key, repository['name'])
+        saveRepositoriesLocally(repository, project, bitBucketGitUrl)
+
+        print "########### Moving to next REPOSITORY! ###########\n\n\n\n\n"
+    break
